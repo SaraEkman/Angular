ff019c677e5d10650616bf8200d71c21ecc697f9
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-01-29 16:45:48 +0100

    Mor angular code to learn

--- testning/src/app/components/todo/todo.component.ts
+++ testning/src/app/components/todo/todo.component.ts
@@ -9,11 +9,41 @@ import { Todo } from 'src/app/models/Todo'
 export class TodoComponent implements OnInit {
   shop: Todo = new Todo('Städa och baka')
 
+  todos: Todo[] = [new Todo('Handla'), new Todo('Diska'), new Todo('sova')]
+  // todoClass: string = "todo";
+
+  image: string = 'someRandomURL';
+
+  isDisabled: boolean = false;
+
+  firstName: string = "";
+
   constructor() {}
 
   ngOnInit(): void { }
 
   toggle():void {
-    this.shop.done = !this.shop.done;
+    // this.shop.done = !this.shop.done;
+    this.isDisabled = !this.isDisabled
+  }
+
+  toggleTodo(i:number) {
+    console.log(i);
+    this.todos[i].done = !this.todos[i].done
+  }
+  toggleTodoOb(toggleTodo: Todo) {
+    toggleTodo.done = !toggleTodo.done
+  }
+
+  handleChange(someVal:string) {
+    this.firstName = someVal
+  }
+
+  search() {
+    console.log(this.firstName);
+
+    this.todos.push(new Todo(this.firstName))
+
+    // this.firstName = "" INTE FUNKAR NU
   }
 }
