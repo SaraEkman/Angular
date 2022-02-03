2cf46fa49027b57e5a5118b6e3f70e5fb75a9536
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-02-03 12:32:04 +0100

    update style

--- inputOutput/src/app/components/print-animal/print-animal.component.ts
+++ inputOutput/src/app/components/print-animal/print-animal.component.ts
@@ -1,27 +1,23 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Animal } from 'src/app/models/Animal';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Animal } from 'src/app/models/Animal'
 
 @Component({
   selector: 'app-print-animal',
   templateUrl: './print-animal.component.html',
-  styleUrls: ['./print-animal.component.scss']
+  styleUrls: ['./print-animal.component.scss'],
 })
 export class PrintAnimalComponent implements OnInit {
   @Input() animal: Animal = new Animal('', '', 0, '')
 
-
   @Output() fed = new EventEmitter<Animal>()
 
-disa: boolean = false;
-  constructor() { }
+  disa: boolean = false
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   save() {
     this.disa = !this.disa
     this.fed.emit(this.animal)
-    
   }
-
 }
