a49a1b4c32513af74df001e89509981e2568d72a
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-02-07 15:20:17 +0100

    Öva direktiv

--- ova-inputOutput/src/app/directives/highlight.directive.ts
+++ ova-inputOutput/src/app/directives/highlight.directive.ts
@@ -5,12 +5,14 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class HighlightDirective {
   @Input('appHighlight') speed: number = 0;
+  @Input() color:string = 'green'
 
   constructor(private el: ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    (this.el.nativeElement as HTMLElement).style.backgroundColor = 'green';
-    (this.el.nativeElement as HTMLElement).style.transition = 'all '+ this.speed +'s';
+    (this.el.nativeElement as HTMLElement).style.backgroundColor = this.color;
+    (this.el.nativeElement as HTMLElement).style.transition =
+      'all ' + this.speed + 's';
   }
 
   @HostListener('mouseleave') onMouseLeave() {
