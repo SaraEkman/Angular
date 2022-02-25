7b3061484bb560dfc07ffb0d508e0c6e84dac517
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-02-25 17:11:47 +0100

    done with getData from API

---
+++ http/src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/models/IProduct';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  products: IProduct[] = [];
+
+  constructor(private service:DataService) { }
+
+  ngOnInit(): void {
+    this.service.getData()
+    this.service.theData$.subscribe((data)=> this.products = data)
+  }
+
+}
