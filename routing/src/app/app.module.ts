b863f8d0c060d721e8207748755f3d9095258877
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-03-02 17:11:47 +0100

    done

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    NotfoundComponent,
+    DetailsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
